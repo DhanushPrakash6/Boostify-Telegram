3af3d756b202f6cb395a682db048378816331a95
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-09-30 20:48:21 +0530

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const uri = "mongodb+srv://Admin:dhanushprakash194@cluster0.jd3xg.mongodb.net/?retryWrites=true&w=majority";
+app.use(cors({
+  origin: [""],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+let client;
+let clientPromise;
+
+if (!client) {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+app.use(bodyParser.json());
+
+app.post('/api/insertUser', async (req, res) => {
+  const { _id, name, coins } = req.body;
+
+  try {
+    const connection = await clientPromise;
+    const db = connection.db("Boostify");
+    const collection = db.collection("Users");
+
+    const existingUser = await collection.findOne({ _id: Number(_id) });
+
+    if (existingUser) {
+      return res.status(200).json({ message: "User already exists" });
+    }
+    const result = await collection.insertOne({ _id, name, coins });
+    res.status(200).json({ message: "User inserted successfully", result });
+  } catch (error) {
+    console.error("Error inserting user data:", error);
+    res.status(500).json({ error: "Error inserting user data" });
+  }
+});
+
+app.get('/api/getUserCoin', async (req, res) => {
+  const _id = req.query.id; 
+
+  if (!_id) {
+    return res.status(400).json({ message: "User ID is required" });
+  }
+
+  try {
+    const connection = await clientPromise;
+    const db = connection.db("Boostify");
+    const collection = db.collection("Users");
+
+    const user = await collection.findOne({ _id: Number(_id) });
+
+    if (user) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json({ coins: user.coins });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error("Failed to fetch user coins:", error);
+    res.status(500).json({ error: "Failed to fetch user coins" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
