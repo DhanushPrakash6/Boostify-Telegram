1cff428255b0beace142ca5ff5d02481a7696e3c
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-10-04 22:20:39 +0530

    Commit

--- Client/src/utils/script/subfile/insta.tsx
+++ Client/src/utils/script/subfile/insta.tsx
@@ -88,7 +88,7 @@ function Insta() {
               headers: {
                 "Content-Type": "application/json",
               },
-              body: JSON.stringify({metrices: requestBody}),
+              body: JSON.stringify(requestBody),
             });
             const result = await response.json();
             
