df5e427ef6a62e2e3c6f0ec7a842026e8002a3e3
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-10-04 22:30:36 +0530

    Commit

--- Client/src/utils/script/component/input.tsx
+++ Client/src/utils/script/component/input.tsx
@@ -9,7 +9,7 @@ const InputField = ({
   min,
   max,
 }) => {
-  const [isChecked, setIsChecked] = useState(isCheckedInitially);
+  const [isChecked, setIsChecked] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
   const handleCheckboxChange = (event) => {
@@ -18,7 +18,7 @@ const InputField = ({
     if (!checked) {
       setInputValue("");
       if (onValueChange) {
-        onValueChange(""); // Notify parent about cleared input
+        onValueChange("");
       }
     }
   };
@@ -27,55 +27,54 @@ const InputField = ({
     const value = event.target.value;
     setInputValue(value);
     if (onValueChange) {
-      onValueChange(value); // Notify parent about input value change
+      onValueChange(value);
     }
   };
-
   const handleInputBlur = () => {
-    if (type === "number") { // Only apply min/max for number inputs
-      let value = Number(inputValue);
-      if (isNaN(value)) {
-        value = ""; // Reset to empty if NaN
-      } else if (value < min) {
-        value = min;
-      } else if (value > max) {
-        value = max;
-      }
-      setInputValue(value.toString());
-      if (onValueChange) {
-        onValueChange(value.toString());
-      }
+    let value = Number(inputValue);
+
+    if (value < min) {
+      value = min;
+    } else if (value > max) {
+      value = max;
+    }
+
+    setInputValue(value.toString());
+    if (onValueChange) {
+      onValueChange(value.toString());
     }
   };
 
   return (
-    <div className="flex flex-row items-center w-full">
-      <div className="w-full">
-        <input
-          className="flip-card__input"
-          placeholder={placeholder}
-          type={type}
-          value={inputValue}
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          disabled={isCheckedInitially ? !isChecked : false}
-          min={type === "number" ? min : undefined} // Set min only for number
-          max={type === "number" ? max : undefined} // Set max only for number
-        />
-      </div>
-      {isCheckedInitially && (
-        <div>
-          <label className="container">
-            <input
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-              type="checkbox"
-            />
-            <div className="checkmark"></div>
-          </label>
+    <>
+      <div className="flex flex-row items-center w-full">
+        <div className="w-full">
+          <input
+            className="flip-card__input"
+            placeholder={placeholder}
+            type={type}
+            value={inputValue}
+            onChange={handleInputChange}
+            onBlur={handleInputBlur}
+            disabled={isCheckedInitially ? !isChecked : false}
+            min={min}
+            max={max}
+          />
         </div>
-      )}
-    </div>
+        {isCheckedInitially && (
+          <div>
+            <label className="container">
+              <input
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                type="checkbox"
+              />
+              <div className="checkmark"></div>
+            </label>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
