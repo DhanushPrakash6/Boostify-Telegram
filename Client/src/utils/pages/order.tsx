f51538e4f6c9a382cf1eb8e65f73fa9b868e447d
DhanushPrakash J <dhanushprakash194@gmail.com>
2024-10-06 18:58:40 +0530

    Commit

--- Client/src/utils/pages/order.tsx
+++ Client/src/utils/pages/order.tsx
@@ -1,29 +1,13 @@
 import "../styles/index.css";
 import { useState, useEffect } from "react";
 import WebApp from "@twa-dev/sdk";
-import {
-  wifi,
-  telegram,
-  spotify,
-  linkedin,
-  whatsapp,
-  facebook,
-  instagram,
-  soundcloud,
-  tiktok,
-  twitch,
-  X,
-  youtube,
-} from "../../icons/Arrow.ts";
 import Home from "./data.tsx";
 import {
-  profile,
-  coin,
-  podium,
-  fund,
-  home,
-  group,
-  wallet,
+  wifi, telegram, spotify, linkedin, whatsapp, facebook,
+  instagram, soundcloud, tiktok, twitch, X, youtube,
+} from "../../icons/Arrow.ts";
+import {
+  profile, coin, podium, fund, home, group, wallet
 } from "../../images/index.ts";
 
 interface UserData {
@@ -36,146 +20,165 @@ interface UserData {
 }
 
 const Orders = () => {
-  const [coinValue, setCoinValue] = useState<number>();
-  const [orders, setOrders] = useState([]);
-  const [userData, setUserData] = useState<UserData | null>(null)
+  const [coinValue, setCoinValue] = useState<number | null>(null);
+  const [orders, setOrders] = useState<any[]>([]);
+  const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      const user = WebApp.initDataUnsafe.user as UserData;
-      setUserData(user)
-    }
+    // Set userData if available from WebApp
+    const user = WebApp.initDataUnsafe.user as UserData | undefined;
+    if (user) setUserData(user);
+
     const fetchUserCoins = async () => {
-        try {
-          // const response = await fetch((WebApp?.initDataUnsafe?.user) ? `https://boostify-server.vercel.app/api/getUserCoin?id=${userData.id}` :
-          //   `https://boostify-server.vercel.app/api/getUserCoin?id=1011111`
-          // );
-          const response = await fetch(`https://boostify-server.vercel.app/api/getUserCoin?id=${user.id}`);
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const data = await response.json();
-          setCoinValue(data.coins.toFixed(2));
-        } catch (error) {
-          console.error("Error fetching user's coins:", error);
-        }
-      };
-    fetchUserCoins();
-    const fetchOrders = async () => {
-        try {
-            // const response = await fetch((WebApp?.initDataUnsafe?.user) ? `https://boostify-server.vercel.app/api/getOrders?id=${userData.id}` :
-            //     `https://boostify-server.vercel.app/api/getOrders?id=1011111`
-            // );
-            const response = await fetch(`https://boostify-server.vercel.app/api/getOrders?id=${user.id}`);
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const data = await response.json();
-            setOrders(data.result); 
-        } catch (error) {
-            console.error("Error fetching Data:", error);
-        }
+      try {
+        const userId = user?.id || 1011111; // Fallback to a default user ID
+        const response = await fetch(
+          `https://boostify-server.vercel.app/api/getUserCoin?id=${userId}`
+        );
+        if (!response.ok) throw new Error("Network response was not ok");
+
+        const data = await response.json();
+        setCoinValue(Number(data.coins).toLocaleString(undefined, {
+          minimumFractionDigits: 2, maximumFractionDigits: 2,
+        }));
+      } catch (error) {
+        console.error("Error fetching user's coins:", error);
+      }
     };
 
-    fetchOrders();
-  }, []);
+    const fetchOrders = async () => {
+      try {
+        const userId = user?.id || 1011111; // Fallback to a default user ID
+        const response = await fetch(
+          `https://boostify-server.vercel.app/api/getOrders?id=${userId}`
+        );
+        if (!response.ok) throw new Error("Network response was not ok");
+
+        const data = await response.json();
+        setOrders(data.result);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
+
+    if (userData) {
+      fetchUserCoins();
+      fetchOrders();
+    }
+  }, [userData]); // Add userData to the dependency array
+
   return (
-    <>
-      <div className="overflow-auto w-full h-full bg-gradient-main p-5 flex flex-col min-h-screen items-center text-black font-medium">
-        <div className="w-full flex justify-end items-center">
-          <div className="w-full flex justify-left items-center">
-            <img src={profile} alt="" className="h-10 w-10" />
-            <Home />
-          </div>
+    <div className="overflow-auto w-full h-full bg-gradient-main p-5 flex flex-col min-h-screen items-center text-black font-medium">
+      <div className="w-full flex justify-end items-center">
+        <div className="w-full flex justify-left items-center">
+          <img src={profile} alt="Profile" className="h-10 w-10" />
+          <Home />
         </div>
-        <div className="w-full flex flex-col justify-center items-center pt-[15px] pb-[15px]">
-          <div className="w-full flex justify-center items-center gap-1">
-            <img
-              src={coin}
-              alt=""
-              className="h-14 w-14 sm:h-16 sm:w-16 md:h-20 md:w-20"
-            />
-            <span className="font-bold text-4xl sm:text-5xl md:text-6xl">
-              {coinValue}
-            </span>
-          </div>
-          <h1 className="w-full flex justify-center items-center text-black text-opacity-50 font-normal text-xl sm:text-2xl md:text-3xl">
-            Available Balance
-          </h1>
-          <div
-            style={{ background: "#ee9ca7" }}
-            className="w-fit h-fit border-2 border-black font-bold rounded-3xl p-3 cursor-pointer mt-5"
-          >
-            <a
-              href="/funds"
-              className="w-full flex justify-center items-center gap-2"
-            >
-              <img
-                src={wallet}
-                alt=""
-                className="h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
-              />
-              <h1 className="font-bold text-black text-opacity-90 text-base sm:text-lg md:text-xl">
-                Add Money
-              </h1>
-            </a>
-          </div>
-        </div>
-        <span className="w-[500%] border-dashed border-t-2 border-black m-6"/>
-        <h1 className="w-full flex justify-left items-center text-black text-opacity-90 font-normal text-l sm:text-2xl md:text-3xl">
-            Transactions
-        </h1>
-        <div className="m-3 w-full h-[410px] pb-[110px] flex flex-col gap-4 overflow-y-scroll no-scrollbar">
-          {orders.length > 0 ? (
-            orders.map((order) => (
-              <div key={order._id} className="w-full border rounded-2xl bg-white flex justify-between items-center p-4">
-                <div className="w-full flex flex-col overflow-x-auto gap-1">
-                  <h1 className="font-normal text-opacity-50 text-[1.7vh] sm:text-2xl md:text-3xl">{order.social}</h1>
-                  <h1 className="text-black text-opacity-50 font-medium text-xs sm:text-2xl md:text-3xl">{order.metrics.postLink}</h1>
-                </div>
-                <div>
-                  <h1 className="text-red-600 font-normal text-lg sm:text-2xl md:text-3xl text-opacity-90">
-                    -{order.amount}$
-                  </h1>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className="w-full flex justify-center items-center">
-              <h1 className="text-black text-opacity-50 font-normal text-sm sm:text-2xl md:text-3xl">
-                No Transactions Found
-              </h1>
-            </div>
-          )}
-        </div>
-        <footer className="w-full flex justify-around items-center p-4 border-dashed border-t-2 border-black">
-          {[
-            { src: home, alt: "Home" },
-            { src: group, alt: "Friends" },
-            { src: podium, alt: "Orders" },
-            { src: fund, alt: "Funds" },
-          ].map((item, index) => (
-            <a
-              key={index}
-              href={`${
-                item.alt === "Home" ? "/" : item.alt === "Friends" ? "/friends" : item.alt === "Orders" ? "/orders" : item.alt === "Funds" ? "/funds" : "/"
-              }`}
-              className={`flex flex-col items-center ${
-                item.alt === "Orders" ? "border-2 border-black" : ""
-              }`}
-            >
-              <img
-                src={item.src}
-                alt={item.alt}
-                className={`h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 ${
-                  item.alt === "Orders" ? "opacity-100" : "opacity-80"
-                }`}
-              />
-            </a>
-          ))}
-        </footer>
       </div>
-    </>
+
+      <div className="w-full flex flex-col justify-center items-center pt-[15px] pb-[15px]">
+        <div className="w-full flex justify-center items-center gap-1">
+          <img
+            src={coin}
+            alt="Coin"
+            className="h-14 w-14 sm:h-16 sm:w-16 md:h-20 md:w-20"
+          />
+          <span className="font-bold text-4xl sm:text-5xl md:text-6xl">
+            {coinValue !== null ? coinValue : "Loading..."}
+          </span>
+        </div>
+        <h1 className="w-full flex justify-center items-center text-black text-opacity-50 font-normal text-xl sm:text-2xl md:text-3xl">
+          Available Balance
+        </h1>
+        <div
+          style={{ background: "#ee9ca7" }}
+          className="w-fit h-fit border-2 border-black font-bold rounded-3xl p-3 cursor-pointer mt-5"
+        >
+          <a
+            href="/funds"
+            className="w-full flex justify-center items-center gap-2"
+          >
+            <img
+              src={wallet}
+              alt="Wallet"
+              className="h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
+            />
+            <h1 className="font-bold text-black text-opacity-90 text-base sm:text-lg md:text-xl">
+              Add Money
+            </h1>
+          </a>
+        </div>
+      </div>
+
+      <span className="w-[500%] border-dashed border-t-2 border-black m-6" />
+      <h1 className="w-full flex justify-left items-center text-black text-opacity-90 font-normal text-l sm:text-2xl md:text-3xl">
+        Transactions
+      </h1>
+
+      <div className="m-3 w-full h-[410px] pb-[110px] flex flex-col gap-4 overflow-y-scroll no-scrollbar">
+        {orders.length > 0 ? (
+          orders.map((order) => (
+            <div
+              key={order._id}
+              className="w-full border rounded-2xl bg-white flex justify-between items-center p-4"
+            >
+              <div className="w-full flex flex-col overflow-x-auto gap-1">
+                <h1 className="font-normal text-opacity-50 text-[1.7vh] sm:text-2xl md:text-3xl">
+                  {order.social}
+                </h1>
+                <h1 className="text-black text-opacity-50 font-medium text-xs sm:text-2xl md:text-3xl">
+                  {order.metrics.postLink}
+                </h1>
+              </div>
+              <div>
+                <h1 className="text-red-600 font-normal text-lg sm:text-2xl md:text-3xl text-opacity-90">
+                  -{order.amount}$
+                </h1>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="w-full flex justify-center items-center">
+            <h1 className="text-black text-opacity-50 font-normal text-sm sm:text-2xl md:text-3xl">
+              No Transactions Found
+            </h1>
+          </div>
+        )}
+      </div>
+
+      <footer className="w-full flex justify-around items-center p-4 border-dashed border-t-2 border-black">
+        {[
+          { src: home, alt: "Home" },
+          { src: group, alt: "Friends" },
+          { src: podium, alt: "Orders" },
+          { src: fund, alt: "Funds" },
+        ].map((item, index) => (
+          <a
+            key={index}
+            href={`${
+              item.alt === "Home"
+                ? "/"
+                : item.alt === "Friends"
+                ? "/friends"
+                : item.alt === "Orders"
+                ? "/orders"
+                : "/funds"
+            }`}
+            className={`flex flex-col items-center ${
+              item.alt === "Orders" ? "border-2 border-black" : ""
+            }`}
+          >
+            <img
+              src={item.src}
+              alt={item.alt}
+              className={`h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 ${
+                item.alt === "Orders" ? "opacity-100" : "opacity-80"
+              }`}
+            />
+          </a>
+        ))}
+      </footer>
+    </div>
   );
 };
 
